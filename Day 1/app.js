94348d4ed23cb828ed853889cd081bfe2b4a6f0c
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-07-16 00:22:58 +0530

    Day 1 Completed

---
+++ Day 1/app.js
@@ -0,0 +1,21 @@
+const removeTransition=(e)=>{
+    if(e.propertyName !== 'transform'){
+        return
+    }
+    e.target.classList.remove('playing')
+}
+
+function playSound (e){
+    console.log(e.key.charCodeAt(0));
+    const audio = this.document.querySelector(`audio[data-key="${e.key.charCodeAt(0)}"]`)
+    const key = this.document.querySelector(`.key[data-key="${e.key.charCodeAt(0)}"]`)
+    key.classList.add("playing")
+    if(!audio){return}
+    audio.currentTime=0
+    audio.play()
+}
+
+
+const keys = Array.from( document.querySelectorAll('.key'))
+keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+window.addEventListener('keydown',playSound)
